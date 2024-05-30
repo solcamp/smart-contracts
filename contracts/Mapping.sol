47aa129372cbd2222eba22dbfbc4b97b83d7665a
solcamp <solcambel610@gmail.com>
2024-05-30 12:38:18 +0300

    mapping

---
+++ contracts/Mapping.sol
@@ -0,0 +1,24 @@
+
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.20;
+
+contract ERC20Token {
+    address public banker = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    mapping(address => uint256) public balances;
+
+    function setSomeonesBalance(address owner, uint256 amount) public {
+        if (msg.sender == banker) {
+            balances[owner] = amount;
+        }
+        // do nothing
+    }
+
+    function transferTokensBetweenAddresses(address sender, address receiver, uint256 amount) public {
+        if (msg.sender == banker) {
+            balances[sender] -= amount;   // deduct/debit the sender's balance
+            balances[receiver] += amount; // credit the reciever's balance
+        }
+        // do nothing
+    }
+}
