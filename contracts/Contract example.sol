f5a940bd2b66a6be0e01b1f15d94a822f1602143
solcamp <solcambel610@gmail.com>
2024-06-07 08:51:08 +0300

    crypto

---
+++ contracts/Contract example.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
+contract ExampleContract {
+
+	function helloWorld() public pure returns (uint256){
+		return 100;
+	}
+	
+	function haloDunia() public pure returns (bool) {
+		return true;
+	}
+}
